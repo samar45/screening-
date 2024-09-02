f443c64573e43b89dc2c1b541993b07473b052c4
Samar Mohanty <81224518+samar45@users.noreply.github.com>
2024-09-02 18:12:08 +0530

    Add files via upload
    Project uploaded

---
+++ Flask/app.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, jsonify
+import pickle
+import numpy as np
+import os
+
+app = Flask(__name__)
+
+# Define the folder where the model is stored
+# folder_name = "Models"
+# path =os.path.join(os.getcwd(),folder_name)
+with(open('DT_Dating_score.pkl',"rb")) as model_file:
+    model = pickle.load(model_file)
+
+@app.route("/")
+def greet():
+
+    return "hello i am samar"
+
+@app.route("/Greet1")
+def greet1():
+    return "Call me MR X"
+
+@app.route("/predict",methods = ["GET"])
+def predict_note_authentication():
+    """
+    ["Gender","PurchasedVIP","Income","Children","Age","Attractiveness"]
+    """
+    input_cols = ["Gender","PurchasedVIP","Income","Children","Age","Attractiveness"]
+
+    list1 = [float(request.form[i]) for i in input_cols]
+    for i in input_cols:
+        val = request.args.get(i)
+        list1.append(eval(val))
+
+    prediction = model.predict([list1])
+    print(prediction)
+    return "Hello The answer is"+str(abs((prediction)))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
